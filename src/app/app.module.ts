825be697637402cb87808c6365107e86b5e9b631
Hao Chen <haoruchen@google.com>
2020-09-29 19:00:21 -0700

    Initial code for json editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
+// import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from '@components/home/home.component';
+
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiPlaygroundComponent } from './components/api-playground/api-playground.component';
-import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { NgJsonEditorModule } from 'ang-jsoneditor';
+import { ShowComponent } from './components/api-playground/show.component';
 
 @NgModule({
-  declarations: [AppComponent, ApiPlaygroundComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    ApiPlaygroundComponent,
+    HomeComponent,
+    ShowComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    NgJsonEditorModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/api-playground/api-playground.component.ts
+++ src/app/components/api-playground/api-playground.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { JsonEditorComponent, JsonEditorOptions } from 'ang-jsoneditor';
+import mockData from '@services/mock-data';
+import jsonUtils from '@utils/api-pg-json-parser';
+import { request } from 'https';
 
 @Component({
   selector: 'app-api-playground',
   templateUrl: './api-playground.component.html',
-  styleUrls: ['./api-playground.component.scss']
+  styleUrls: ['./api-playground.component.scss'],
 })
-export class ApiPlaygroundComponent implements OnInit {
+export class ApiPlaygroundComponent {
+  @ViewChild('editor') editor: JsonEditorComponent;
+  @ViewChild('editorMult') editorMult: JsonEditorComponent;
 
-  constructor() { }
+  showData;
+  schemas = mockData.schemas;
+  options = new JsonEditorOptions();
+  data: any;
 
-  ngOnInit(): void {
+  constructor() {
+    const gRequest =
+      mockData.resources.datastore.resources.interactions.resources.internal.methods.bridgeTrackRouterSubmitAction.request;
+
+    this.data = jsonUtils.unpackRequest(gRequest, this.schemas);
+    console.log(this.data);
+    this.options.language = 'en';
+    this.options.mode = 'tree';
+    this.options.modes = this.options.modes = ['form', 'tree', 'view'];
+    this.options.statusBar = this.options.statusBar = false;
   }
 
+  showJson(data): any {
+    return JSON.stringify(data, null, 2);
+  }
+
+  changeLog(event = null): void {
+    this.showData = this.editorMult.get();
+  }
 }
